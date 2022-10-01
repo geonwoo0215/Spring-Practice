1941cdf189d91e397f3000f384af8f570d0ba927
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-10-01 22:50:25 +0900

    test : spring security 테스트

---
+++ forwarding-redirect/src/test/java/com/forwardingredirect/forwardingredirect/ForwardingRedirectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forwardingredirect.forwardingredirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForwardingRedirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
